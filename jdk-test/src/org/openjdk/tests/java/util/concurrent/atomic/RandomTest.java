75730b78440452e86743919e763c5d8c5d05a98d
Peter Levart <Peter.Levart@marand.si>
2013-01-16 09:26:59 +0100

    remove unused + alternative TLR

--- jdk-test/src/org/openjdk/tests/java/util/concurrent/atomic/RandomTest.java
+++ jdk-test/src/org/openjdk/tests/java/util/concurrent/atomic/RandomTest.java
@@ -1,38 +1,19 @@
 package org.openjdk.tests.java.util.concurrent.atomic;
 
 import java.util.Random;
-import java.util.concurrent.MultiThreadedRandom;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.atomic.LongAdder;
+import java.util.function.Function;
 
 /**
  */
 public class RandomTest
 {
-    static class RandomWorker extends Thread
-    {
-        private final Random rnd;
-        private final int loops;
 
-        RandomWorker(Random rnd, int loops)
-        {
-            this.rnd = rnd;
-            this.loops = loops;
-        }
-
-        @Override
-        public void run()
-        {
-            for (int i = 0; i < loops; i++)
-                rnd.nextInt();
-        }
-    }
-
-    static class TLRandomWorker extends Thread
+    static class TLRCurrentNextIntWorker extends Thread
     {
         private final int loops;
 
-        TLRandomWorker(int loops)
+        TLRCurrentNextIntWorker(int loops)
         {
             this.loops = loops;
         }
@@ -45,11 +26,29 @@ public class RandomTest
         }
     }
 
-    static Object[] test(int threads, int loops, Random rnd)
+    static class TLRNextIntWorker extends Thread
+    {
+        private final int loops;
+
+        TLRNextIntWorker(int loops)
+        {
+            this.loops = loops;
+        }
+
+        @Override
+        public void run()
+        {
+            Random rnd = ThreadLocalRandom.current();
+            for (int i = 0; i < loops; i++)
+                rnd.nextInt();
+        }
+    }
+
+    static Object[] test(Function<Integer, Thread> workerFactory, int threads, int loops)
     {
         Thread[] workers = new Thread[threads];
         for (int i = 0; i < threads; i++)
-            workers[i] = rnd == null ? new TLRandomWorker(loops) : new RandomWorker(rnd, loops);
+            workers[i] = workerFactory.apply(loops);
         long t0 = System.nanoTime();
         for (Thread worker : workers) worker.start();
         try
@@ -60,28 +59,35 @@ public class RandomTest
         {
             throw new RuntimeException(e);
         }
-        return new Object[] { System.nanoTime() - t0 };
+        return new Object[]{System.nanoTime() - t0};
     }
 
-    static void testX(int threads, int loops, Random rnd)
+    static void testX(int threads, int loops)
     {
-        System.out.printf("%20s ", rnd == null ? "ThreadLocalRandom" : rnd.getClass().getSimpleName());
-        System.out.printf("%4d threads x %9d loops :", threads, loops);
+        System.out.printf("\n%4d threads x %9d loops...\n\n", threads, loops);
+
+        // warm-up
+        for (int i = 0; i < 5; i++)
+            test(TLRCurrentNextIntWorker::new, threads, loops);
+        for (int i = 0; i < 5; i++)
+            test(TLRNextIntWorker::new, threads, loops);
+
+        System.out.printf("TLR.current().nextInt(): ");
         for (int i = 0; i < 7; i++)
-            System.out.printf(" %,15d ns", test(threads, loops, rnd));
+            System.out.printf(" %,15d ns", test(TLRCurrentNextIntWorker::new, threads, loops));
+        System.out.println();
+
+        System.out.printf("          tlr.nextInt(): ");
+        for (int i = 0; i < 7; i++)
+            System.out.printf(" %,15d ns", test(TLRNextIntWorker::new, threads, loops));
         System.out.println();
     }
 
     public static void main(String[] args)
     {
-        Random rnd = new MultiThreadedRandom();
-        testX(1, 100_000_000, null);
-        testX(1, 100_000_000, rnd);
-        testX(2, 100_000_000, null);
-        testX(2, 100_000_000, rnd);
-        testX(4, 100_000_000, null);
-        testX(4, 100_000_000, rnd);
-        testX(6, 100_000_000, null);
-        testX(6, 100_000_000, rnd);
+        testX(1, 100_000_000);
+        testX(2, 100_000_000);
+        testX(4, 100_000_000);
+        testX(6, 100_000_000);
     }
 }
